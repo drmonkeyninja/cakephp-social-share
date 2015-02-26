0aefb36d42a0172868dfde4458d6911ee64826dc
Andy Carter <andy1704@gmail.com>
2015-02-26 21:39:12 +0000

    Added support for Twitter and passing a title to the share link.

--- Test/Case/View/Helper/SocialShareHelperTest.php
+++ Test/Case/View/Helper/SocialShareHelperTest.php
@@ -15,8 +15,28 @@ class SocialShareHelperTest extends CakeTestCase {
 
 	public function testLinks() {
 
+		// Facebook test
 		$expected = '<a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fexample.com">Share</a>';
-		$this->assertEquals($expected, $this->SocialShare->link('facebook', 'Share', 'http://example.com'));
+		$this->assertEquals(
+			$expected,
+			$this->SocialShare->link(
+				'facebook',
+				'Share',
+				'http://example.com'
+			)
+		);
+
+		// Twitter test
+		$expected = '<a href="http://twitter.com/home?status=Foo+bar+http%3A%2F%2Fexample.com">Share</a>';
+		$this->assertEquals(
+			$expected,
+			$this->SocialShare->link(
+				'twitter',
+				'Share',
+				'http://example.com',
+				array('title' => 'Foo bar')
+			)
+		);
 
 	}
 
--- View/Helper/SocialShareHelper.php
+++ View/Helper/SocialShareHelper.php
@@ -8,15 +8,16 @@ class SocialShareHelper extends AppHelper {
 
 	protected $_urls = array(
 		'facebook' => 'https://www.facebook.com/sharer/sharer.php?u={url}',
-		'gplus' => 'https://plus.google.com/share?url={url}'
+		'gplus' => 'https://plus.google.com/share?url={url}',
+		'twitter' => 'http://twitter.com/home?status={title}+{url}'
 	);
 
-	public function shareUrl($service, $url = null) {
+	public function shareUrl($service, $url = null, $options = array()) {
 
 		// If the URL hasn't been set, get the current full path.
 		$url = empty($url) ? Router::url(null, true) : $url;
 
-		$title = '';
+		$title = !empty($options['title']) ? $options['title'] : '';
 
 		if (!empty($this->_urls[$service])) {
 			return preg_replace(
@@ -26,7 +27,7 @@ class SocialShareHelper extends AppHelper {
 				),
 				array(
 					urlencode($url),
-					$title
+					urlencode($title)
 				),
 				$this->_urls[$service]
 			);
@@ -36,12 +37,37 @@ class SocialShareHelper extends AppHelper {
 
 	}
 
-	public function link($service, $text, $url = null, $options = array()) {
+
+	public function link($service, $text, $url = null, $attributes = array()) {
+
+		$options = array();
+
+		if (!empty($attributes['title'])) {
+			$options['title'] = $attributes['title'];
+			unset($attributes['title']);
+		}
 
 		return $this->Html->link(
 			$text,
-			$this->shareUrl($service, $url),
-			$options
+			$this->shareUrl($service, $url, $options),
+			$attributes
+		);
+
+	}
+
+
+/**
+ * Returns only array entries listed in a whitelist
+ *
+ * @param array $array original array to operate on
+ * @param array $whitelist keys you want to keep
+ * @return array
+ */
+	protected function _arrayWhitelist($array, $whitelist) {
+
+		return array_intersect_key(
+			$array, 
+			array_flip($whitelist)
 		);
 
 	}
