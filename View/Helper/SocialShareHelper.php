4bf32c2e216c8a4f4f6b2d498ca6af30938cea48
Andy Carter <andy1704@gmail.com>
2015-03-01 09:59:40 +0000

    Added phpdoc block comments.

--- View/Helper/SocialShareHelper.php
+++ View/Helper/SocialShareHelper.php
@@ -6,6 +6,12 @@ class SocialShareHelper extends AppHelper {
 
 	public $helpers = array('Html');
 
+
+/**
+ * An array of services and their corresponding share/bookmarking URLs.
+ *
+ * @var array
+ */
 	protected $_urls = array(
 		'facebook' => 'https://www.facebook.com/sharer/sharer.php?u={url}',
 		'gplus' => 'https://plus.google.com/share?url={url}',
@@ -13,6 +19,21 @@ class SocialShareHelper extends AppHelper {
 		'twitter' => 'http://twitter.com/home?status={title}+{url}'
 	);
 
+
+/**
+ * Creates a share URL.
+ *
+ * ### Options
+ *
+ * - `title` Text to be passed to service relating to the shared content(e.g. page title).
+ * 
+ * For other options see HtmlHelper::link().
+ *
+ * @param string $service Social Media service to create share link for.
+ * @param string|array $url Cake-relative URL or array of URL parameters, or external URL (starts with http://)
+ * @param array $options Array of options.
+ * @return string An URL.
+ */
 	public function href($service, $url = null, $options = array()) {
 
 		// Get the URL, get the current full path if a URL hasn't been specified.
@@ -39,6 +60,15 @@ class SocialShareHelper extends AppHelper {
 	}
 
 
+/**
+ * Creates an HTML link to share a URL.
+ *
+ * @param string $service Social Media service to create share link for.
+ * @param string $text The content to be wrapped by <a> tags.
+ * @param string|array $url Cake-relative URL or array of URL parameters, or external URL (starts with http://)
+ * @param array $attributes Array of options and HTML attributes.
+ * @return string An `<a />` element.
+ */
 	public function link($service, $text, $url = null, $attributes = array()) {
 
 		$options = array();
