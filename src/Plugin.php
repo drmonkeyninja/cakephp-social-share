83d399e05ba055464bcd3e3cde24a98a8f38f532
Andy Carter <andy1704@gmail.com>
2020-07-27 21:11:02 +0100

    Update for CakePHP 4

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace SocialShare;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\PluginApplicationInterface;
+use Cake\Http\MiddlewareQueue;
+use Cake\Routing\RouteBuilder;
+
+/**
+ * Plugin for Foo
+ */
+class Plugin extends BasePlugin
+{
+}
